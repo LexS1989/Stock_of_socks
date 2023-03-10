24549cf339de1b9135d12aa874074e6f817237d2
lexS1989 <lexshadrin@mail.ru>
2023-03-10 11:21:09 +1100

    New project

---
+++ src/main/java/ru/skypro/stock_of_socks/StockOfSocksApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.stock_of_socks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOfSocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockOfSocksApplication.class, args);
+    }
+
+}
